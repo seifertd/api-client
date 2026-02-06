eda189c4945481ad8878fea41aa7645ff88b2123
Douglas A. Seifert <doug@dseifert.net>
2026-02-05 22:06:35 -0800

    remove versioned api client and Cakefile

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 ApiClient = require("./lib/api_client");
-VersionedApiClient = require("./lib/versioned_api_client");
 
 module.exports = {
   ApiClient: ApiClient,
-  VersionedApiClient: VersionedApiClient
 }
