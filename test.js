b2c52ad93e6572888eda7b559af0edcd320b4210
Douglas A. Seifert <doug@dseifert.net>
2026-02-06 15:24:50 -0800

    Add post method and tests

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ import { ApiClient } from './lib/api_client.js';
 
 const client = ApiClient.create("dseifert");
 
-const r = await client.get({});
+const r = await client.get("bookmarks.html")
 
 console.log("RESPONSE: ", r);
--- test/api_client.test.js
+++ test/api_client.test.js
@@ -4,6 +4,7 @@ chai.use(spies);
 const { expect } = chai;
 import { ApiClient } from '../lib/api_client.js';
 import config from 'config';
+import http from 'http';
 
 class Foo extends ApiClient {}
 
@@ -82,9 +83,78 @@ describe("ApiClient", _ => {
         port: 80
       });
     });
-    it("has the default api path of /", () => {
-      expect(endpoint.url()).to.equal("http://test.com:80/")
-      expect(endpoint.url({foo: 'bar'})).to.equal("http://test.com:80/?foo=bar")
+    it("normalizes null path", () => {
+      expect(endpoint.url(null)).to.equal("http://test.com:80/")
+      expect(endpoint.url(null, {foo: 'bar'})).to.equal("http://test.com:80/?foo=bar")
+    });
+    it("normalizes / path", () => {
+      expect(endpoint.url("/")).to.equal("http://test.com:80/")
+      expect(endpoint.url("/", {foo: 'bar'})).to.equal("http://test.com:80/?foo=bar")
+    });
+    it("normalizes non empty path", () => {
+      expect(endpoint.url("more")).to.equal("http://test.com:80/more")
+      expect(endpoint.url("/more", {foo: 'bar'})).to.equal("http://test.com:80/more?foo=bar")
+    });
+  });
+  describe("Endpoint with basepath", () => {
+    let endpoint = null;
+    before(() => {
+      endpoint = new ApiClient({
+        host: 'test.com',
+        port: 80,
+        options: { base_path: "/foo" }
+      });
+    });
+    it("normalizes null path", () => {
+      expect(endpoint.url(null)).to.equal("http://test.com:80/foo")
+      expect(endpoint.url(null, {foo: 'bar'})).to.equal("http://test.com:80/foo?foo=bar")
+    });
+    it("Normalizes non-null path", () => {
+      expect(endpoint.url("stuff")).to.equal("http://test.com:80/foo/stuff")
+      expect(endpoint.url("/stuff")).to.equal("http://test.com:80/foo/stuff")
+    });
+  });
+  describe("Basic ApiClient", () => {
+    let server = null;
+    let client = null;
+    let reqHdrs = null;
+    let reqUrl = null;
+    let reqMeth = null;
+    before(() => {
+      server = http.createServer((req, res) => {
+        reqHdrs = req.headers;
+        reqUrl = req.url;
+        reqMeth = req.method;
+        res.writeHead(200, { 'Content-Type': 'text/json' });
+        res.end('{"sigma":"alpha"}');
+      });
+      server.listen(8888, 'localhost', () => {});
+      ApiClient.registerConfig('localhost', {
+        host: "localhost",
+        port: "8888"
+      });
+      client = ApiClient.create('localhost');
+    });
+    after(() => {
+      server.close(() => {});
+    });
+    it("Can make a get request", async () => {
+      const response = await client.get("/", {foo: 'bar'}, {"Header1": "value1"});
+      expect(response.status).to.equal(200);
+      expect(client.url("/", {foo: "bar"})).to.include(reqUrl);
+      expect(reqHdrs['header1']).to.equal("value1");
+      expect(reqMeth).to.equal('GET');
+      const resJson = await response.json();
+      expect(resJson).to.eql({sigma: "alpha"});
+    });
+    it("Can make a post request", async () => {
+      const response = await client.post("/", {foo: 'bar'}, "post body", {"Header1": "value1", "Content-Type": "text/plain"});
+      expect(response.status).to.equal(200);
+      expect(client.url("/", {foo: "bar"})).to.include(reqUrl);
+      expect(reqHdrs['header1']).to.equal("value1");
+      expect(reqMeth).to.equal('POST');
+      const resJson = await response.json();
+      expect(resJson).to.eql({sigma: "alpha"});
     });
   });
 });
