15164a37a757ff9fb5db5866b549c05e1c81ace3
Douglas A. Seifert <doug@dseifert.net>
2026-02-06 15:27:54 -0800

    Get rid of chai-spies

--- test/api_client.test.js
+++ test/api_client.test.js
@@ -1,6 +1,4 @@
 import * as chai from 'chai';
-import spies from 'chai-spies';
-chai.use(spies);
 const { expect } = chai;
 import { ApiClient } from '../lib/api_client.js';
 import config from 'config';
